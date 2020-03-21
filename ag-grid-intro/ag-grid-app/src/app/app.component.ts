3ce01f6192e5b5f6a25beb4952b7ed004dc7e2eb
mustafa esen <esenmustafa42@gmail.com>
2020-03-22 01:59:47 +0300

    Angular ag-grid

---
+++ ag-grid-intro/ag-grid-app/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component,OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AgGridAngular} from 'ag-grid-angular';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+@ViewChild('agGrid',{static:false}) agGrid: AgGridAngular;
+
+  title = 'ag-grid-app';
+
+  columnDefs = [
+{headerName : "Make",field: "make" , rowGroup:true},
+{headerName : "Price",field: "price" }
+ ];
+
+ autoGroupColumnDef = {
+headerName:'Model',
+field:'model',
+cellRenderer:'agGroupCellRenderer',
+cellRendererParams:{
+checkbox : true
+
+}
+
+ }
+rowData :  any;
+
+constructor(private http: HttpClient){}
+ngOnInit()
+{
+this.rowData = this.http.get("https://api.myjson.com/bins/15psn9");
+}
+getSelectedRows(){
+  const selectedNodes = this.agGrid.api.getSelectedNodes();
+  const selectedData = selectedNodes.map(node => node.data);
+  const selectedDataStringPresentation = selectedData.map(node => node.make + ' '  + node.model).join(", ");
+  alert(`selected Nodes:${selectedDataStringPresentation}`); 
+}
+}
